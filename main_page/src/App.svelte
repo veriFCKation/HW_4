0aac7c935f5f34c12381481ad93da39426a75385
veriFCKation <k.shchekina@innopolis.university>
2022-06-24 16:05:06 +0300

    new commit

--- main_page/src/App.svelte
+++ main_page/src/App.svelte
@@ -1,29 +1,68 @@
 <script lang="ts">
- import Projects from "./projects.svelte";
+	import AboutMe from "./aboutMe.svelte";
+	import Contacts from "./contacts.svelte";
+	import Projects from "./projects.svelte";
+	//export let name: string;
 
-	export let name: string;
+	let src1 = 'https://i.pinimg.com/originals/bd/f0/25/bdf025165c835a6c22eced2a83fd49c3.jpg';
+
+	async function findDog(){
+		const response = await fetch("https://dog.ceo/api/breeds/image/random");
+		const data = await response.text();
+		if (response.ok){
+			document.getElementById('new-photo').style.display = 'block';
+		}
+		console.log(data);//data.message
+		src1 = data;
+	}
+
 </script>
 
 <main>
+	<h1> Hello, my name is Ksenia</h1>
+	
+	<div id = "my-photo">
+		<img src = {'https://i.pinimg.com/originals/bd/f0/25/bdf025165c835a6c22eced2a83fd49c3.jpg'}>
+		<p> I haven't any good photo, so, look at this cute dog </p>
+	</div>
+	
+	<button on:click = {findDog}> 
+		I think, this dog looks lonely 
+	</button>
+	
+	<div id = "new-photo" style = 'display:none'>
+		<img style = "width:230px" src = {src1} alt = "random-dog">
+		<p> Ok, let's find a friend! </p>
+	</div>
+	
+	<AboutMe/>
 	<Projects/>
+	<Contacts/>
 
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
 	main {
+		background-color: rgb(7, 3, 19);
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
 
+	*{
+		color: #66FF00;
+	}
+
 	h1 {
-		color: #ff3e00;
+		color: #4CBB17;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	h2{
+		color: #489315;
 	}
 
 	@media (min-width: 640px) {
---
+++ main_page/src/aboutMe.svelte
@@ -0,0 +1,4 @@
+<div>
+    <h2 id = "about-me"> About me </h2>
+    <p> I just start learning front-end and I already like it! I know C++ and base of Java and Python, but HTML looks very different. My CSS skills just go brrrrrr (yet, I hope). All this is new to me, but I will improve my skills and make this site better. </p>
+</div>
